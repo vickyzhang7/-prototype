7891b27176e1e0d683b2ce361c4a40152bfbd858
weiqi <weiqi997998@gmail.com>
2024-10-12 00:45:07 -0400

    Layout

--- src/components/LayoutManager.js
+++ src/components/LayoutManager.js
@@ -1,19 +1,54 @@
 import React from 'react';
 import GridLayout from 'react-grid-layout';
+import { Responsive, WidthProvider } from 'react-grid-layout';
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
 
+// 使用 WidthProvider 使得 GridLayout 能够自适应宽度
+const ResponsiveGridLayout = WidthProvider(Responsive);
+
 const LayoutManager = ({ children }) => {
-  const layout = [
-    { i: 'table', x: 0, y: 0, w: 6, h: 10 },
-    { i: 'chart', x: 6, y: 0, w: 6, h: 10 }
-  ];
+  const breakpoints = { lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 };
+  
+
+  const cols = { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 };
+
+  
+  const layouts = {
+    lg: [
+      { i: 'table', x: 0, y: 0, w: 6, h: 10 },
+      { i: 'chart', x: 6, y: 0, w: 6, h: 10 }
+    ],
+    md: [
+      { i: 'table', x: 0, y: 0, w: 10, h: 10 },
+      { i: 'chart', x: 0, y: 1, w: 10, h: 10 }
+    ],
+    sm: [
+      { i: 'table', x: 0, y: 0, w: 6, h: 10 },
+      { i: 'chart', x: 0, y: 1, w: 6, h: 10 }
+    ],
+    xs: [
+      { i: 'table', x: 0, y: 0, w: 4, h: 10 },
+      { i: 'chart', x: 0, y: 1, w: 4, h: 10 }
+    ],
+    xxs: [
+      { i: 'table', x: 0, y: 0, w: 2, h: 10 },
+      { i: 'chart', x: 0, y: 1, w: 2, h: 10 }
+    ]
+  };
 
   return (
-    <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+    <ResponsiveGridLayout
+      className="layout"
+      layouts={layouts}
+      breakpoints={breakpoints}
+      cols={cols}
+      rowHeight={30}
+      width={1200}
+    >
       <div key="table">{children[0]}</div>
       <div key="chart">{children[1]}</div>
-    </GridLayout>
+    </ResponsiveGridLayout>
   );
 };
 
